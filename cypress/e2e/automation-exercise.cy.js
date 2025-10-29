981a80fa6198099d4c586123b28a375115ec9b3d
allexandresampaio <allexandresss@gmail.com>
2025-10-29 10:08:39 -0300

    fixtures + enviando arquivos no form

--- cypress/e2e/automation-exercise.cy.js
+++ cypress/e2e/automation-exercise.cy.js
@@ -107,4 +107,17 @@ describe ('Automation Exercise', () => {
         cy.contains('button', 'Signup').click()
         cy.get('.signup-form > form > p').should('have.text', 'Email Address already exist!')
     })
+
+    it ('Enviar um formulário de Contato', () => {
+        cy.get('a[href="/contact_us"]').click()
+        cy.get('[data-qa="name"]').type(userData.name)
+        cy.get('[data-qa="email"]').type(userData.email)
+        cy.get('[data-qa="subject"]').type(userData.subject)
+        cy.get('[data-qa="message"]').type(userData.message)
+        cy.fixture('example.json').as('arquivo')
+        cy.get('input[type=file]').selectFile('@arquivo')
+        cy.get('[data-qa="submit-button"]').click()
+        cy.get('.status').should('have.text', 'Success! Your details have been submitted successfully.')
+
+    })
 })
