96156e96eb289f99fd7719dda4284278eee82cf3
allexandresampaio <allexandresss@gmail.com>
2025-10-31 18:32:05 -0300

    incluindo retries caso testes falhem

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  retires:{
+    openMode: 0,
+    runMOde: 2
+  },
+
   e2e: {
     reporter: 'cypress-mochawesome-reporter',
     supportFile: 'cypress/support/e2e.js', // confirme este caminho
